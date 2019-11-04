ad46d454b2dc1ab4f5591cbd1afd38519fc911f1
iMac <imac@iMac-iMac.local>
2019-11-04 16:34:15 +0200

    add map

--- app/src/main/java/com/d2/pcu/fragments/map/TempleFragment.java
+++ app/src/main/java/com/d2/pcu/fragments/map/TempleFragment.java
@@ -2,6 +2,7 @@ package com.d2.pcu.fragments.map;
 
 import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.Manifest;
@@ -18,17 +19,23 @@ import android.view.ViewGroup;
 import com.d2.pcu.R;
 import com.d2.pcu.databinding.TempleFragmentBinding;
 import com.d2.pcu.fragments.BaseFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
 import java.lang.reflect.Array;
 
-public class TempleFragment extends BaseFragment {
+public class TempleFragment extends BaseFragment implements OnMapReadyCallback {
 
     private static final String TAG = TempleFragment.class.getSimpleName();
 
     private TempleFragmentBinding binding;
     private TempleViewModel viewModel;
 
-    private static final String[] PERMISSIONS = new String[] { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION };
+    private static final String[] PERMISSIONS = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
     private static final int REQUEST_CODE = 201;
 
     public static TempleFragment newInstance() {
@@ -44,11 +51,38 @@ public class TempleFragment extends BaseFragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_container);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         viewModel = ViewModelProviders.of(this).get(TempleViewModel.class);
         binding.setLifecycleOwner(getViewLifecycleOwner());
         binding.setModel(viewModel);
+
+        if (checkPermission()) {
+            viewModel.loadData();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            if (checkPermission()) {
+                viewModel.loadData();
+            }
+        }
     }
 
     private boolean checkPermission() {
@@ -62,4 +96,16 @@ public class TempleFragment extends BaseFragment {
         return true;
     }
 
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        viewModel.getLocation().observe(getViewLifecycleOwner(), new Observer<LatLng>() {
+            @Override
+            public void onChanged(LatLng latLng) {
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11f));
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/d2/pcu/fragments/map/TempleViewModel.java
+++ app/src/main/java/com/d2/pcu/fragments/map/TempleViewModel.java
@@ -1,7 +1,41 @@
 package com.d2.pcu.fragments.map;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class TempleViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.annotation.NonNull;
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+
+import com.d2.pcu.data.model.map.Temple;
+import com.d2.pcu.utils.Locator;
+import com.google.android.gms.maps.model.LatLng;
+
+public class TempleViewModel extends AndroidViewModel {
+
+    private Temple model = new Temple();
+    private Locator locator;
+
+    private LiveData<LatLng> location;
+
+    public TempleViewModel(@NonNull Application application) {
+        super(application);
+
+        locator = new Locator(getApplication().getApplicationContext());
+    }
+
+    void loadData() {
+        location = Transformations.switchMap(locator.getCurrentLocation(), new Function<LatLng, LiveData<LatLng>>() {
+            @Override
+            public LiveData<LatLng> apply(LatLng input) {
+                return new MutableLiveData<>(input);
+            }
+        });
+    }
+
+    LiveData<LatLng> getLocation() {
+        return location;
+    }
 }
