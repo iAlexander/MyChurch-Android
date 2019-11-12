c31345b664ebea659abf42db62d69ef82ece46b5
iMac <imac@iMac-iMac.local>
2019-11-12 13:30:20 +0200

    change base url

--- app/src/main/java/com/d2/pcu/utils/Constants.java
+++ app/src/main/java/com/d2/pcu/utils/Constants.java
@@ -2,7 +2,7 @@ package com.d2.pcu.utils;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://ec2-3-133-104-185.us-east-2.compute.amazonaws.com:3000/";
+    public static final String BASE_URL = "http://ec2-3-133-104-185.us-east-2.compute.amazonaws.com:8081/";
 
     public static final int TEMPLE_TYPE_CATHEDRAL = 1;
     public static final int TEMPLE_TYPE_CHURCH = 2;
